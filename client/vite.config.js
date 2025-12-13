71ddc12cd5896c7653962e894c25c7e68af1e393
chsiao <chsiao3@my.centennialcollege.ca>
2025-12-12 20:17:00 -0500

    fix  Azure deployment path (froentend folder)

--- client/vite.config.js
+++ client/vite.config.js
@@ -19,6 +19,6 @@ export default defineConfig({
     },
     build: {
         emptyOutDir: true,
-        outDir: "../dist/app",
+        outDir: "dist/app",
     },
 });
--- server/express.js
+++ server/express.js
@@ -21,7 +21,7 @@ const CURRENT_WORKING_DIR = process.cwd();
 app.use(cors({
   origin: 'http://localhost:5173',  // Allow frontend to access from this domain
 }));
-app.use(express.static(path.join(CURRENT_WORKING_DIR, "dist/app")));
+app.use(express.static(path.join(CURRENT_WORKING_DIR, "client/dist/app")));//for test environment 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/auth", authRoutes);
