70771ec8fcb7a279d4905ee7ac0e66ebbbbfff0c
chsiao <chsiao3@my.centennialcollege.ca>
2026-03-17 10:41:47 -0400

    mock redis url

--- server/config/redis.js
+++ server/config/redis.js
@@ -1,26 +1,15 @@
 // server/config/redis.js
-import { createClient } from "redis";
-import config from "../config/config.js";
-
-const redis = createClient({
-    url: config.redisUrl,
-    socket: {
-        tls: false,
-    }
-});
-
-redis.on("connect", () => {
-    console.log("Redis connected");
-});
-
-redis.on("error", (err) => {
-    console.error("Redis error", err);
-});
+const redis = {
+    on: () => { },
+    get: async () => null,
+    set: async () => null,
+    del: async () => null,
+    isOpen: true,
+    connect: async () => { console.log("Redis "); }
+};
 
 export const connectRedis = async () => {
-    if (!redis.isOpen) {
-        await redis.connect();
-    }
+    await redis.connect();
 };
 
 export default redis;
